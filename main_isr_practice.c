ee97c9b2cc08a602655547a1e0fdcd4ca01a11ff
Enzo <enzopj@al.insper.edu.br>
2025-02-19 13:58:08 -0300

    practice

--- main_isr_practice.c
+++ main_isr_practice.c
@@ -6,18 +6,24 @@ const int BTN_PIN_R = 28;
 
 // ERRO: cppcheck unitvar
 // ERRO: addon IsrVarVolatile
-int btn_flag;
+volatile int btn_flag;
 
 void btn_callback(uint gpio, uint32_t events) {
-  if (events == 0x4) { // fall edge
+    if (events == 0x4) { // fall edge
+
+        btn_flag = 1;
+    }
+}
+
+void atualiza(void) {
 
     // ERRO: addon IsrPrintf
     printf("btn pressed \n");
 
     // ERRO: addon IsrNoLoop
     while (!pio_get(BTN_PIN_R)) {
-      // ERRO: addon IsrNoDelay
-      sleep_ms(1);
+        // ERRO: addon IsrNoDelay
+        sleep_ms(1);
     }
 
     // ERRO: addon IsrPrintf
@@ -25,28 +31,26 @@ void btn_callback(uint gpio, uint32_t events) {
 
     // ERRO: addon IsrNoDelay
     sleep_ms(1);
-    btn_flag = 1;
-  }
 }
 
 int main() {
-  stdio_init_all();
-  gpio_init(BTN_PIN_R);
-  gpio_set_dir(BTN_PIN_R, GPIO_IN);
-  gpio_pull_up(BTN_PIN_R);
-  gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
-                                     &btn_callback);
+    stdio_init_all();
+    gpio_init(BTN_PIN_R);
+    gpio_set_dir(BTN_PIN_R, GPIO_IN);
+    gpio_pull_up(BTN_PIN_R);
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
+                                       &btn_callback);
 
-  volatile int capture_flag = 0;
-  int a;
-  while (1) {
-    if (btn_flag) {
-      capture_flag = 1;
-      btn_flag = 0;
-    }
+    volatile int capture_flag = 0;
+    int a;
+    while (1) {
+        if (btn_flag) {
+            capture_flag = 1;
+            btn_flag = 0;
+        }
 
-    if (capture_flag) {
+        if (capture_flag) {
+            atualiza();
+        }
     }
-    
-  }
 }
